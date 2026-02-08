bbd19d7219c15d0b39f803cc897480cdbc790314
Camilo G. <cgalvis.sierra@gmail.com>
2026-02-08 18:14:33 -0500

    Correct method name and enhance error handling
    Fix method name and improve error handling in csvExport.

--- cnn/Array.py
+++ cnn/Array.py
@@ -7,16 +7,22 @@ class Array:
 
     # Accepts twp parameters, the filepath of the image and the label (0/1) of the image
     # Concatenates the result to the the dataframe
-    def attatchToArray(self, dataTuple):
+    def attachToArray(self, dataTuple):
         try:
             newRow = {"filepath": dataTuple[0], "label": dataTuple[1]}
             dataFrame = pd.DataFrame([newRow])
             self.df = pd.concat([self.df, dataFrame], ignore_index=True)
+            return True
         except Exception as e:
             print(f"Error attatching to array: {e}")
+            return False
 
     #Data is an array which index 0 is the label(Y/N) of the imag
     #the rest is the pixel values of the image
     def csvExport(self):
-        return self.df.to_csv("data/dataset.csv",index=False)
+        try:
+            self.df.to_csv("data/dataset.csv",index=False)
+            return True
+        except Exception as e:
+            return False
         
