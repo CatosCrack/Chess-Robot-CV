eb1db8ca8d62fd56ea2a9a5d2eb1d227b813040d
Emilie Stewart <118950755+em-stew@users.noreply.github.com>
2026-02-08 18:21:08 -0500

    Update cnn/Array.py

--- cnn/Array.py
+++ cnn/Array.py
@@ -2,27 +2,16 @@ import pandas as pd
 
 class Array:
     def __init__(self, df):
-        self.df = pd.DataFrame(columns=['filepath', 'label'])
-        return self.df
+        if df is None:
+            self.df = pd.DataFrame(columns=['img', 'label'])
+        else:
+            self.df = df
 
-    # Accepts twp parameters, the filepath of the image and the label (0/1) of the image
-    # Concatenates the result to the the dataframe
-    def attachToArray(self, dataTuple):
+    def attatchToArray(filepath, label):
         try:
-            newRow = {"filepath": dataTuple[0], "label": dataTuple[1]}
+            newRow = {"filepath": filepath, "label": label}
             dataFrame = pd.DataFrame([newRow])
-            self.df = pd.concat([self.df, dataFrame], ignore_index=True)
-            return True
+            data = pd.concat([data, dataFrame], ignore_index=True)
         except Exception as e:
             print(f"Error attatching to array: {e}")
-            return False
-
-    #Data is an array which index 0 is the label(Y/N) of the imag
-    #the rest is the pixel values of the image
-    def csvExport(self):
-        try:
-            self.df.to_csv("data/dataset.csv",index=False)
-            return True
-        except Exception as e:
-            return False
         
